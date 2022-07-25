23e81df468bf82478d0cc46df481ba77658080d2
Shonda Darron <sdarron@at-d6.msk.21-school.ru>
2022-07-25 17:37:38 +0300

    25.07.22

--- src/commands/INVITE.cpp
+++ src/commands/INVITE.cpp
@@ -3,5 +3,10 @@
 void INVITE(Command *command) {
 
   std::vector<std::string> args = command->get_arguments();
+
+  
+  // User& kicked_user = command->get_server().get_user_by_nickname(args[1]);
+  // add_user(User& user);
+  // get_username() ;
   
 }
--- src/commands/LIST.cpp
+++ src/commands/LIST.cpp
@@ -2,36 +2,37 @@
 
 void LIST(Command *command) {
 
-    // Параметры: [<channel>{,<channel>} [<server>]]
-    // используется для вывода списка канало и их топиков. Если
-    // используется параметр <channel>, то выводится только статус этого
-    // канала. Приватные каналы указаны (без их топиков) как каналы "Prv", без
-    // указания количества клиентов, сидящих на этом канале. 
+    // TODO
+    // 
     // секретные каналы не приводятся в списке,
     // если, конечно, клиент не является членом подобного канала.
-    
-    std::vector<std::string> args = command->get_arguments();
-    irc::IrcServer::t_channel_list& channels = command->get_server().get_channels();    
-    
-    if (channels.empty())
-       return command->reply(ERR_NOSUCHSERVER);
+        //  402     ERR_NOSUCHSERVER
+        //                "<server name> :No such server"
 
+        //        - Используется для сообщения, что сервер, указанный в
+        //          строке параметров, не найден.
+        //if (!server)
+    //   return command->reply(ERR_NOSUCHSERVER);
+
+    std::vector<std::string> args = command->get_arguments();
+    irc::IrcServer& server = command->get_server();
+  
+    irc::IrcServer::t_channel_list& channels = server.get_channels();    
     command->reply(RPL_LISTSTART);
     
-    if (args.empty()){
-        for (int i = 0; ;++i)
+    if (!args.size()){
+        for( irc::IrcServer::t_channel_list::iterator channel = channels.begin(); channel != channels.end(); ++channel)
         {
-            irc::IrcServer::t_channel_list::iterator channel = std::find(channels.begin(), channels.end(), args[i]);
-            command->reply(RPL_LIST, channel->get_name());
-            if (channel == channels.end())
-                 break;
+            if(!channel->is_private()){
+                std::cout <<  channel->get_name() << std::endl;
+                command->reply(RPL_LIST, channel->get_name(), channel->get_topic());
+            }
         }  
     }
     else{
         irc::IrcServer::t_channel_list::iterator channel = std::find(channels.begin(), channels.end(), args[0]);
-        command->reply(RPL_LIST, channel->get_name(), channel->get_topic());
+        command->reply(RPL_LIST, channel->get_topic());
     }
     command->reply(RPL_LISTEND);
     return;
-
 }
